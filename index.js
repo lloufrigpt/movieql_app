a4affbbb16126c20869b41320dc4590053b75b6b
lloufrigpt <lloufrigpt@gmail.com>
2021-03-11 15:28:35 +0900

    # with client

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ import { GraphQLServer } from "graphql-yoga";
 import resolvers from "./graphql/resolvers";
 
 const server = new GraphQLServer({
-    // schema
-    // 사용자에게 보내거나 사용자로부터 받을 데이터에 관한 설명
-
-    typeDefs: "graphql/schema.graphql",
+    typeDefs: `type Movie {
+        id: Int!
+        title: String!
+        rating: Float
+        description_intro: String
+        language: String
+        medium_cover_image: String
+        genres: [String]
+    }
+    
+    type Query {
+        movies(limit: Int, rating: Float): [Movie]!
+        movie(id: Int!): Movie
+        suggestions(id: Int!): [Movie]!
+    }
+    `,
     resolvers
-
 });
 
-server.start(() => console.log("Server Running"));
+server.start(() => console.log("Graphql Server Running"));
